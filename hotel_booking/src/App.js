7282e58adcce2dceb6f08a221b381887a19e49ed
pritigarud93 <pritigarud93@gmail.com>
2021-07-29 10:26:31 +0530

    temporary changes

--- hotel_booking/src/App.js
+++ hotel_booking/src/App.js
@@ -1,13 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
-import Header from './Header';
+import logo from "./logo.svg";
+import "./App.css";
+import Header from "./Header";
 
-function App() {
+/* function App() {
   return (
     <div>
       <Header></Header>
     </div>
   );
-}
+} */
+import React, { useState } from "react";
+
+const App = () => {
+  const [count, setCount] = useState(0);
+
+  const incrementCount = () => {
+    setCount(count + 1);
+  };
+
+  return (
+    <div>
+      <h1>{count}</h1>
+      <input type="button" onClick={incrementCount} />
+    </div>
+  );
+};
 
 export default App;
